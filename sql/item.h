MariaDB/server

Add FRM parser utility
- Parse FRM files
- Parse FRM files created in Engines like ARCHIVE, MEMORY, MyISAM, INNODB, CSV, ARIA
- Parse FRM files with default expressions like default(val)
- Parse FRM files that contain mathematical expressions like 1 * 2 * 3
- Parse FRM files containing sequences (exception are frm files of DDL expressions like CREATE SEQUENCE ...)
- Parse FRM files for some plugin functions like JSON (exception: INET and complex geometric functions are not yet supported)
- Provides a --debug option to stack trace in case of failures

Tests for following features are added:
- simple columns, at least int, text, varchar(value)
- keys, using btree
- keys using rtree
- sinple vcol expressions: a int, x int as (a *2)
- same for DEFAULT: a int default (whatever)
- DEFAULT CURRENT_TIMESTAMP is a special case
- field-level comments, table-level comments
- innodb engine
- more exotic engines
- engine options
- partitions -- currently failing so added with error clause
- Expressions from plugins, like json, Item_geometry_func, inet6 -- currently failing so added with error clause
- Sequences: default(nextval(s))
- Foreign keys in innodb

- Applied aggressive linker flags to reduce executable size:
 - Used -Wl, --gc-sections to remove unused sections
 - Applied, -Wl,--strip-all to remove debugging symbols
 - Used -fno-common to remove duplicate symbol error

Reusing existing functions from table.cc to implement this FRM Parser util

sql/item.h
@@ -3211,6 +3211,73 @@ public:
 };
 
 
+#ifdef FRM_PARSER
+/**
+  Frm_parser_item - A minimal Item subclass for FRM parsing
+  
+  This class stores an expression string without parsing or executing it.
+  Used by the standalone FRM parser to avoid SQL parsing crashes.
+*/
+class Frm_parser_item: public Item
+{
+  String *expr_str;
+  uint expr_length;
+  
+public:
+  Frm_parser_item(THD *thd, const char *expr, uint length) : Item(thd)
+  {
+    expr_str = new String();
+    expr_str->copy(expr, length, &my_charset_utf8mb3_general_ci);
+    expr_length = length;
+  }
+  
+  enum Type type() const override { return CONST_ITEM; }
+  
+  void print(String *str, enum_query_type query_type) override
+  {
+    if (!expr_str || expr_str->length() == 0) {
+      return;
+    }
+
+    const char* prefix = "PARSE_VCOL_EXPR ";
+    const uint prefix_len = 16;
+
+    if (expr_str->length() > prefix_len &&
+        memcmp(expr_str->ptr(), prefix, prefix_len) == 0) {
+      str->append(expr_str->ptr() + prefix_len,
+                  expr_str->length() - prefix_len);
+        } else {
+          str->append(expr_str->ptr(), expr_str->length());
+        }
+  }
+  
+  const char *full_name() const { return "frm_parser_expression"; }
+  
+  double val_real() override { return 0.0; }
+  longlong val_int() override { return 0; }
+  String *val_str(String *) override { return expr_str; }
+  void fix_length_and_dec() {}
+  Item *clone_item(THD* thd) const override{ return new(get_thd_memroot(thd)) Frm_parser_item(thd, expr_str->ptr(), expr_length); }
+  
+  const Type_handler *type_handler() const override{ return &type_handler_varchar; }
+  my_decimal *val_decimal(my_decimal *) override{ return NULL; }
+  bool get_date(THD *thd, MYSQL_TIME *ltime, date_mode_t fuzzydate) override
+  { 
+    bzero((char*) ltime, sizeof(*ltime));
+    return 1; 
+  }
+  Field *create_tmp_field_ex(MEM_ROOT *root, TABLE *table, Tmp_field_src *src,
+                             const Tmp_field_param *param) override
+  {
+    return tmp_table_field_from_field_type_maybe_null(root, table, src, param, false);
+  }
+  Item *do_get_copy(THD *thd) const override
+  { return new(get_thd_memroot(thd)) Frm_parser_item(thd,expr_str->ptr(), expr_length); }
+  Item *do_build_clone(THD *thd) const override { return do_get_copy(thd); }
+};
+#endif
+
+
 /*****************************************************************************
   The class is a base class for representation of stored routine variables in
   the Item-hierarchy. There are the following kinds of SP-vars:

hp77-creator/my-maria-server

sql/item.h
@@ -3225,6 +3225,80 @@ public:
 };
 
 
+#ifdef FRM_PARSER
+/**
+  Frm_parser_item - A minimal Item subclass for FRM parsing
+  
+  This class stores an expression string without parsing or executing it.
+  Used by the standalone FRM parser to avoid SQL parsing crashes.
+*/
+class Frm_parser_item: public Item
+{
+  String *expr_str;
+  uint expr_length;
+
+protected:
+  Item *shallow_copy(THD *thd) const override
+  {
+    return new(get_thd_memroot((thd)) Frm_parser_item(thd, expr_str->ptr(), expr_length);
+  }
+
+  Item *deep_copy(THD *thd) const override
+  {
+    return shallow_copy(thd); // For FRM parser, shallow copy is sufficient
+  }
+  
+public:
+  Frm_parser_item(THD *thd, const char *expr, uint length) : Item(thd)
+  {
+    expr_str = new String();
+    expr_str->copy(expr, length, &my_charset_utf8mb3_general_ci);
+    expr_length = length;
+  }
+  
+  enum Type type() const override { return CONST_ITEM; }
+  
+  void print(String *str, enum_query_type query_type) override
+  {
+    if (!expr_str || expr_str->length() == 0) {
+      return;
+    }
+
+    const char* prefix = "PARSE_VCOL_EXPR ";
+    const uint prefix_len = 16;
+
+    if (expr_str->length() > prefix_len &&
+        memcmp(expr_str->ptr(), prefix, prefix_len) == 0) {
+      str->append(expr_str->ptr() + prefix_len,
+                  expr_str->length() - prefix_len);
+        } else {
+          str->append(expr_str->ptr(), expr_str->length());
+        }
+  }
+  
+  const char *full_name() const { return "frm_parser_expression"; }
+  
+  double val_real() override { return 0.0; }
+  longlong val_int() override { return 0; }
+  String *val_str(String *) override { return expr_str; }
+  void fix_length_and_dec() {}
+
+  const Type_handler *type_handler() const override{ return &type_handler_varchar; }
+  my_decimal *val_decimal(my_decimal *) override{ return NULL; }
+  bool get_date(THD *thd, MYSQL_TIME *ltime, date_mode_t fuzzydate) override
+  { 
+    bzero((char*) ltime, sizeof(*ltime));
+    return 1; 
+  }
+  Field *create_tmp_field_ex(MEM_ROOT *root, TABLE *table, Tmp_field_src *src,
+                             const Tmp_field_param *param) override
+  {
+    return tmp_table_field_from_field_type_maybe_null(root, table, src, param, false);
+  }
+};
+#endif
+
+
 /*****************************************************************************
   The class is a base class for representation of stored routine variables in
   the Item-hierarchy. There are the following kinds of SP-vars: